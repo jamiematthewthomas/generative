c2d573b11f52f642edde72eafe35932d5b27cfad
Jamie Thomas <jamie.matthew.thomas@gmail.com>
2025-04-13 18:37:44 +0100

    Rename sketch

--- src/main/java/dev/jamiethomas/generative/sketches/SketchDrawImageUsingStrokes.java
+++ src/main/java/dev/jamiethomas/generative/sketches/SketchDrawImageUsingStrokes.java
@@ -7,7 +7,7 @@ import dev.jamiethomas.generative.utils.ImageUtils;
 import dev.jamiethomas.generative.utils.StrokeUtils;
 import processing.core.PApplet;
 
-public class SketchDrawImageUsingCurves extends GenerativeSVG {
+public class SketchDrawImageUsingStrokes extends GenerativeSVG {
 
 	@Override
 	public void setup() {
@@ -17,8 +17,8 @@ public class SketchDrawImageUsingCurves extends GenerativeSVG {
 
 	@Override
 	public void draw() {
-	  BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/lake_compressed.png");
-//		BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/cat_nerd_compressed.png");
+//	  BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/lake_compressed.png");
+		BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/cat_nerd_compressed.png");
 
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; j < 100; j++) {
@@ -27,8 +27,8 @@ public class SketchDrawImageUsingCurves extends GenerativeSVG {
 				int g = (rgb >> 8) & 0xFF;
 				int b = (rgb & 0xFF);
 				int grey = (r + g + b) / 3;
-				StrokeUtils.upwardsBellCurve(this, i*10, j*10, 10, (255 - grey) / 11);
-//				StrokeUtils.hundredsAndThousands(this, i*10, j*10, 10, 10, (255 - grey) / 25, 10);
+//				StrokeUtils.upwardsBellCurve(this, i*10, j*10, 10, (255 - grey) / 11);
+				StrokeUtils.hundredsAndThousands(this, i*10, j*10, 10, 10, (255 - grey) / 25, 10);
 			}
 		}
 		
@@ -36,8 +36,8 @@ public class SketchDrawImageUsingCurves extends GenerativeSVG {
 	}
 
   public static void main(String[] args) {
-    String[] processingArgs = { SketchDrawImageUsingCurves.class.getSimpleName() };
-    SketchDrawImageUsingCurves mySketch = new SketchDrawImageUsingCurves();
+    String[] processingArgs = { SketchDrawImageUsingStrokes.class.getSimpleName() };
+    SketchDrawImageUsingStrokes mySketch = new SketchDrawImageUsingStrokes();
     PApplet.runSketch(processingArgs, mySketch);
   }
 	
