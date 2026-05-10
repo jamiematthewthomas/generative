a888f28a3f18a10d0adc52bdbf0aa2fd66b27c55
Jamie Thomas <jamie.matthew.thomas@gmail.com>
2026-05-10 13:37:13 +0100

    Local changes after hiatus

--- src/main/java/dev/jamiethomas/generative/sketches/SketchDrawImageUsingStrokes.java
+++ src/main/java/dev/jamiethomas/generative/sketches/SketchDrawImageUsingStrokes.java
@@ -17,22 +17,22 @@ public class SketchDrawImageUsingStrokes extends Generative {
 	@Override
 	public void drawWithSVGExport() {
 	  noFill();
-	  
-//	  BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/lake_compressed.png");
-		BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/cat_nerd_compressed.png");
+
+	  BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/lake_compressed.png");
+//		BufferedImage image = ImageUtils.loadImageFromFile("C:/Users/jamie/Desktop/cat_nerd_compressed.png");
 
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; j < 100; j++) {
 				int rgb = image.getRGB(i, j);
-				int r = (rgb >> 16) & 0xFF; 
+				int r = (rgb >> 16) & 0xFF;
 				int g = (rgb >> 8) & 0xFF;
 				int b = (rgb & 0xFF);
 				int grey = (r + g + b) / 3;
-//				StrokeUtils.upwardsBellCurve(this, i*10, j*10, 10, (255 - grey) / 11);
-				StrokeUtils.hundredsAndThousands(this, i*10, j*10, 10, 10, (255 - grey) / 25, 10);
+				StrokeUtils.upwardsBellCurve(this, i*10, j*10, 10, (255 - grey) / 11);
+//				StrokeUtils.hundredsAndThousands(this, i*10, j*10, 10, 10, (255 - grey) / 25, 10);
 			}
 		}
-		
+
 	}
 
   public static void main(String[] args) {
@@ -40,5 +40,5 @@ public class SketchDrawImageUsingStrokes extends Generative {
     SketchDrawImageUsingStrokes mySketch = new SketchDrawImageUsingStrokes();
     PApplet.runSketch(processingArgs, mySketch);
   }
-	
+
 }
